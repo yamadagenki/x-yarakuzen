6135362c66dbf3c212254238d551f720594d8727
shengbo.xu <to.be.mr.all.rounder@gmail.com>
2018-02-02 10:29:19 +0900

    add: get params

--- lib/x/yarakuzen/translate.rb
+++ lib/x/yarakuzen/translate.rb
@@ -32,6 +32,10 @@ module X
         puts post_with_sign(text)
       end
 
+      def get_data(custom_data)
+        puts get_with_sign(custom_data)
+      end
+
       private
 
       def initialize_https(uri)
@@ -52,6 +56,39 @@ module X
         OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA1.new, @api_secret_key, "#{timestamp}#{@api_pub_key}")
       end
 
+      def get_with_sign(custom_data)
+        uri = URI.parse(API_URL)
+        nonce = get_timestamp
+        signature = get_signature(nonce)
+
+        body = {
+          publicKey: @api_pub_key,
+          timestamp: nonce,
+          signature: signature,
+          customData: custom_data,
+          count: 10 
+        }
+
+        begin
+          req = Net::HTTP::Get.new(uri)
+          req.form_data = body
+
+          https = initialize_https(uri)
+          https.start do |w|
+            response = w.request(req)
+            case response
+            when Net::HTTPSuccess
+              json = JSON.parse(response.body)
+              return json
+            else
+              raise 'Failed to connect to Yarakuzen: ' + response.body
+            end
+          end
+        rescue StandardError
+          raise
+        end
+      end
+
       def post_with_sign(text)
         uri = URI.parse(API_URL)
         nonce = get_timestamp
@@ -64,8 +101,10 @@ module X
           lcSrc: @origin_lang,
           lcTgt: @target_lang,
           tier: @tier,
+          persist: 0,
           "texts[0][customData]": nonce,
-          "texts[0][text]": text
+          "texts[0][text]": text,
+          machineTranslate: 1
         }
 
         begin
